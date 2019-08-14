6f5453b600a73251fffbb2e0a9d759f2245a6e2e
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2019-08-13 19:39:55 -0700

    added srevice worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+var dataCacheName = 'template-pwa';
+var cacheName = 'template-pwa';
+var filesToCache = [
+  // Uncomment the block below when you are ready to cache files. Feel free to add any other files that
+  // you wish to cache. Be warned that caching files requires that you clear the cache after making updates.
+  /*
+  '/',
+ "./fonts",
+ "./images",
+ "./icons",
+ "./index.html",
+ "./manifest.json",
+ */
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
